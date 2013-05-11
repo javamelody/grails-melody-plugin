214782965b029e84a485e771699af6bdece8d40c
evernat <evernat@free.fr>
2013-05-12 00:36:40 +0200

    jump plugin version to be in sync with javamelody version

--- GrailsMelodyGrailsPlugin.groovy
+++ GrailsMelodyGrailsPlugin.groovy
@@ -7,7 +7,7 @@ import net.bull.javamelody.Parameters
 
 class GrailsMelodyGrailsPlugin {
     // the plugin version
-    def version = "1.21"
+    def version = "1.44"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.4 > *"
     // the other plugins this plugin depends on
