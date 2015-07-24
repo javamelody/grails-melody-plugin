324f356f9ea5d2fac3a329f67b9332430808cc9d
evernat <evernat@free.fr>
2015-07-24 18:22:16 +0200

    javamelody moved to github

--- src/samples/GrailsMelodyConfig.groovy
+++ src/samples/GrailsMelodyConfig.groovy
@@ -1,6 +1,6 @@
 /*
 You can find all detailed parameter usage from
-http://code.google.com/p/javamelody/wiki/UserGuide#6._Optional_parameters
+https://github.com/javamelody/javamelody/wiki/UserGuide#6-optional-parameters
 Any parameter with 'javamelody.' prefix configured in this file will be add as init-param of java melody MonitoringFilter.
  */
 
