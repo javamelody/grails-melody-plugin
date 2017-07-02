3ab721dd915b5b6e8b8480343b7d7eb2f52d36ae
evernat <evernat@free.fr>
2017-07-03 01:16:45 +0200

    1.68.0

--- src/main/groovy/grails/melody/plugin/MelodyInterceptorEnhancer.groovy
+++ src/main/groovy/grails/melody/plugin/MelodyInterceptorEnhancer.groovy
@@ -17,7 +17,7 @@ class MelodyInterceptorEnhancer {
         //to 'intercept' method call and collect infomation for monitoring purpose.
         //The code below mimics 'MonitoringSpringInterceptor.invoke()'
         def SPRING_COUNTER = MonitoringProxy.getSpringCounter()
-        final boolean DISABLED = GrailsMelodyUtil.getGrailsMelodyConfig(grailsApplication)?.javamelody?.disabled || Boolean.parseBoolean(Parameters.getParameter(Parameter.DISABLED))
+        final boolean DISABLED = GrailsMelodyUtil.getGrailsMelodyConfig(grailsApplication)?.javamelody?.disabled || Parameter.DISABLED.getValueAsBoolean()
 
         if (DISABLED || Parameters.isCounterHidden(SPRING_COUNTER.getName())) {
             if (DISABLED) {
